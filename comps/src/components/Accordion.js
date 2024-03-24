97cebb7b670b76bc4863ac36546baf4cba2de74b
Liam <liam.obrien@snapiot.com>
2024-03-24 16:44:53 -0700

    Enabled each label header to be clicked and then the content section appears. Used mapping function

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -6,11 +6,9 @@ function Accordion({ items }) {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
-        const content = isExpanded && <div>{item.content}</div>;
-
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
                 {isExpanded &&  <div>{item.content}</div>}
             </div>
         );
