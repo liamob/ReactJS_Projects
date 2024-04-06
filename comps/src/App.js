ac90843ffdfab41e2211a4aa9aa7c090e577e9f2
Liam <liam.obrien@snapiot.com>
2024-04-06 14:45:10 -0700

    Finsihed dropdown ui element

--- comps/src/App.js
+++ comps/src/App.js
@@ -15,11 +15,13 @@ function App() {
     ]
 
     return (
-        <Dropdown 
-            options={options} 
-            value={selection} 
-            onChange={handleSelect} 
-        />
+        <div className="flex">
+            <Dropdown 
+                options={options} 
+                value={selection} 
+                onChange={handleSelect} 
+            />
+        </div>
     );
 }
 
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,7 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+            if(!divEl.current) {
+                return;
+            }
+
+            if (!divEl.current.contains(event.target)) {
+                setIsOpen(false);
+            } 
+        };
+
+        document.addEventListener('click', handler, true);
+
+        return () => {
+            document.removeEventListener('click', handler); 
+        };
+    }, []);
 
     const handleClick = () => {
         setIsOpen(!isOpen);
@@ -16,18 +37,23 @@ function Dropdown({ options, value, onChange }) {
 
     const renderedOptions = options.map((option) => {
         return (
-            <div onClick={() => handelOptionClick(option)} key={option.value}>
+            <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handelOptionClick(option)} key={option.value}>
                 {option.label}
             </div>
         );
     });
 
     return (
-        <div>
-            <div onClick={handleClick}>
+        <div ref={divEl} className="w-48 relative">
+            <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || 'Select...'}
-            </div>
-            {isOpen && <div>{renderedOptions}</div>}
+                <GoChevronDown className="text-lg" />
+            </Panel>
+            {isOpen && (
+                <Panel className="absolute top-full">
+                    {renderedOptions}
+                </Panel>
+            )}
         </div>
     );
 }
