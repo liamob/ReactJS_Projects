63d1b45a7b9170e4912a1f1af987c66544f18c52
Liam <liam.obrien@snapiot.com>
2024-11-07 12:21:51 -0700

    Adding table and counter tabs and code in those tabs.

--- comps/src/components/Sidebar.js
+++ comps/src/components/Sidebar.js
@@ -2,10 +2,12 @@ import Link from './Link';
 
 function Sidebar() {
     const links = [
-        {label: 'Dropdown', path: '/'},
-        {label: 'Accordion', path: '/accordion'},
-        {label: 'Buttons', path: '/buttons'},
-        {label: 'Modal', path: '/modal'}
+        { label: 'Dropdown', path: '/'},
+        { label: 'Accordion', path: '/accordion'},
+        { label: 'Buttons', path: '/buttons'},
+        { label: 'Modal', path: '/modal'},
+        { label: 'Table', path: '/table'},
+        { label: 'Counter', path: '/counter'},
     ];
 
     const renderedLinks = links.map((link) => {
